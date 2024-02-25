fe25fa4949107c103c7d91161f720d2bdd18c59a
BEAST <110054039+BEASTgg@users.noreply.github.com>
2024-02-25 19:44:54 +0530

    Update checkup.js

--- checkup.js
+++ checkup.js
@@ -11,14 +11,14 @@ let maxNameLen = 0;
 
 for (let i = 4; i < rows.length; i++) {
     try {
-        let secStr = rows[i][1], rollStr = rows[i][2], nameStr = rows[i][4], leavesStr = rows[i][7]; // Fixed variable name 'leavesStr'
+        let secStr = rows[i][1], rollStr = rows[i][2], nameStr = rows[i][4], leavesStr = rows[i][7];
         secStr = secStr.substring(1, secStr.length - 1);
         rollStr = rollStr.substring(1, rollStr.length - 1);
         nameStr = nameStr.substring(1, nameStr.length - 1);
         leavesStr = leavesStr.substring(1, leavesStr.length - 1);
         if (Number(leavesStr) > 12) {
             debarredFromTerm.push({name: nameStr, sec: secStr, roll: rollStr, leaves: leavesStr});
-            maxNameLen = (maxNameLen < nameStr.length) ? nameStr.length : maxNameLen; // Fixed maxNameLen assignment
+            maxNameLen = (maxNameLen < nameStr.length) ? nameStr.length : maxNameLen;
         }
     } catch (error) {
         console.error("Error processing row:", error);
@@ -27,9 +27,4 @@ for (let i = 4; i < rows.length; i++) {
 }
 
 maxNameLen += 10;
-
-// Uncomment the following line to debug the data and see if it's correctly processed
-// console.log(debarredFromTerm);
-
-// Ensure that BarGraphRepresentation is properly implemented and being called with correct parameters
 BarGraphRepresentation(debarredFromTerm);
