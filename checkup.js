b6b4c638c0d165b50b427b978eef463986c9938f
BEAST <110054039+BEASTgg@users.noreply.github.com>
2024-02-25 19:38:33 +0530

    Update checkup.js

--- checkup.js
+++ checkup.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 const { TabularRepresentation } = require('./tabularRep.js');
 const { BarGraphRepresentation } = require('./barGraphRep.js');
 
@@ -6,28 +6,30 @@ const csvData = fs.readFileSync('data.csv', 'utf-8');
 
 const rows = csvData.split('\n').map(row => row.split(','));
 
-var debarredFromTerm = []
-let maxNameLen = 0
+var debarredFromTerm = [];
+let maxNameLen = 0;
+
 for (let i = 4; i < rows.length; i++) {
     try {
-        let secStr = rows[i][1], rollStr = rows[i][2], nameStr = rows[i][4], leaveStr = rows[i][7]
-        secStr = secStr.substring(1, secStr.length - 1)
-        rollStr = rollStr.substring(1, rollStr.length - 1)
-        nameStr = nameStr.substring(1, nameStr.length - 1)
-        leavesStr = leavesStr.substring(1, leavesStr.length - 1)
+        let secStr = rows[i][1], rollStr = rows[i][2], nameStr = rows[i][4], leavesStr = rows[i][7]; // Fixed variable name 'leavesStr'
+        secStr = secStr.substring(1, secStr.length - 1);
+        rollStr = rollStr.substring(1, rollStr.length - 1);
+        nameStr = nameStr.substring(1, nameStr.length - 1);
+        leavesStr = leavesStr.substring(1, leavesStr.length - 1);
         if (Number(leavesStr) > 12) {
-            debarredFromTerm.push({name: nameStr, sec: secStr, roll: rollStr, leaves: leavesStr})
-            maxNameLen = (maxNameLen < nameStr.length) ? maxNameLen = nameStr.length : maxNameLen
+            debarredFromTerm.push({name: nameStr, sec: secStr, roll: rollStr, leaves: leavesStr});
+            maxNameLen = (maxNameLen < nameStr.length) ? nameStr.length : maxNameLen; // Fixed maxNameLen assignment
         }
     } catch (error) {
+        console.error("Error processing row:", error);
         continue;
     }
 }
 
-maxNameLen += 10
+maxNameLen += 10;
 
-// TabularRepresentation(maxNameLen, debarredFromTerm)
+// Uncomment the following line to debug the data and see if it's correctly processed
+// console.log(debarredFromTerm);
 
-setTimeout(() => {
-    BarGraphRepresentation(debarredFromTerm)
-}, 1000)
+// Ensure that BarGraphRepresentation is properly implemented and being called with correct parameters
+BarGraphRepresentation(debarredFromTerm);
