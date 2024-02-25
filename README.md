61fa8e593b7a752fad0bba8e0d9d412a3c1d0766
BEAST <110054039+BEASTgg@users.noreply.github.com>
2024-02-25 17:28:38 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sem-Debarred-List-Processor
+
+Some File handling and basic data representations performed on the list of the students debarred from 5th sem exam. 
+
+Disclaimer: This made this only to learn and practice, not taking it personally is appreciated.
+
+## Setup
+
+In your local development environment, clone the repo and at the root directory path, run command -
+
+```bash
+    npm run dev
+```
+
+## Bar Graph
+
+The bar graph will look like -
+
+![Local Image](./images/bar-graph.png)
---
+++ checkup.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const { TabularRepresentation } = require('./tabularRep.js');
+const { BarGraphRepresentation } = require('./barGraphRep.js');
+
+const csvData = fs.readFileSync('data.csv', 'utf-8');
+
+const rows = csvData.split('\n').map(row => row.split(','));
+
+var debarredFromTerm = []
+let maxNameLen = 0
+for (let i = 4; i < rows.length; i++) {
+    try {
+        let secStr = rows[i][1], rollStr = rows[i][2], nameStr = rows[i][4], leaveStr = rows[i][7]
+        secStr = secStr.substring(1, secStr.length - 1)
+        rollStr = rollStr.substring(1, rollStr.length - 1)
+        nameStr = nameStr.substring(1, nameStr.length - 1)
+        leavesStr = leavesStr.substring(1, leavesStr.length - 1)
+        if (Number(leavesStr) > 12) {
+            debarredFromTerm.push({name: nameStr, sec: secStr, roll: rollStr, leaves: leavesStr})
+            maxNameLen = (maxNameLen < nameStr.length) ? maxNameLen = nameStr.length : maxNameLen
+        }
+    } catch (error) {
+        continue;
+    }
+}
+
+maxNameLen += 10
+
+// TabularRepresentation(maxNameLen, debarredFromTerm)
+
+setTimeout(() => {
+    BarGraphRepresentation(debarredFromTerm)
+}, 1000)
+
+console.log(debarredFromTerm)
